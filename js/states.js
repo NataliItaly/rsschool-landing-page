7c66852de3d1b8928095168b7dfd547df81eb486
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-09-23 00:32:21 +0200

    feat: add local storage for current theme

---
+++ js/states.js
@@ -0,0 +1,20 @@
+const currentTheme = {
+  theme: 'light',
+};
+
+export function getTheme() {
+  return JSON.parse(localStorage.getItem('theme')) || currentTheme.theme;
+}
+
+export function setTheme(value) {
+  currentTheme.theme = value;
+  localStorage.setItem('theme', JSON.stringify(value));
+  document.body.classList.remove(`${value === 'light' ? 'dark' : 'light'}`);
+  document.body.classList.add(value);
+
+  const themeBtns = document.querySelectorAll('.header__toggle-btn');
+  themeBtns.forEach((btn) => {
+    if (btn.id !== value) btn.classList.remove('header__toggle-active');
+    else btn.classList.add('header__toggle-active');
+  });
+}
