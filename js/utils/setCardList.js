2481d9b3392a9476dbe3469eb5923da7bb2060a1
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-09-23 01:26:19 +0200

    feat: add burger menu

--- js/utils/setCardList.js
+++ js/utils/setCardList.js
@@ -1,7 +1,7 @@
 import setCard from './setCard.js';
 
 export default function setCardList(parent, key, data) {
-  const cards = data.map((item, i) => setCard(key, item, i));
+  const cards = data.map((item, i) => setCard(key, item, i)).join('');
 
   parent.insertAdjacentHTML('afterbegin', cards);
 }
